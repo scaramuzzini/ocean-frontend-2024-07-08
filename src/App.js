57661c6e84fa793e02290c58cbc94d8ea7130b52
Mario Torres <scaramuzzini@gmail.com>
2024-07-08 11:36:45 -0400

    Carrinho de compras

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function ItemDoCarrinho({nomeDoItem, descricaoDoItem}) {
   return (
-    <div className="App">
+    <>
+      <h1>
+        Item: {nomeDoItem}
+      </h1>
+      <h4>
+        Descrição do Item: {descricaoDoItem}
+      </h4>
+    </>
+  )
+}
+
+
+function Saudacao({nome}) {
+  return (
+    <h1>
+      Olá, {nome}
+    </h1>
+  )
+}
+
+function OceanApp() {
+  return (
+    <div>
+      <h1> App Ocean</h1>
+    </div>
+  )
+}
+
+function App() {
+
+  let item1 = "Panela";
+  let item2 = "colher";
+
+  return (
+    <div className="1App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Meu primeiro projeto react</h1>
+        <ItemDoCarrinho nomeDoItem={item1} />
       </header>
+      
     </div>
   );
 }
